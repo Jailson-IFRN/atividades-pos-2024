9d3eae4475e6805f5678e9255a9dac432a17caa5
Jailson <jg094151@gmail.com>
2024-08-09 11:52:25 -0300

    WIP - Tarefa JSon

--- parse/imobiliaria.py
+++ parse/imobiliaria.py
@@ -1,6 +1,7 @@
 from xml.dom.minidom import parse
 import json
 
+
 imobiliaria = parse("imobiliaria.xml")
 
 
@@ -9,18 +10,65 @@ vari = imobiliaria.documentElement
 
 imoveis = vari.getElementsByTagName('imovel')
 
+
 data_imobiliaria = []
-data_proprietario = []
+data_proprietario = {}
+emails ={}
+telefones = {}
+endereco2 = {}
+caracteristicas2 = {}
+descricao = {}
+valor = {}
 for i in imoveis:
-    descricao = i.getElementsByTagName("descricao")[0].firstChild.nodeValue
+    descricao1 = i.getElementsByTagName("descricao")[0].firstChild.nodeValue
+    descricao["descricao"] = descricao1
+    valor1 = i.getElementsByTagName("valor")[0].firstChild.nodeValue
+    valor["valor"] = valor1
+    data_imobiliaria.append(descricao)
+    data_imobiliaria.append(valor)
     elemento_proprietario = i.getElementsByTagName("proprietario")
     for j in elemento_proprietario:
         nome = j.getElementsByTagName("nome")[0].firstChild.nodeValue
-    #proprietario = elemento_proprietario.firstChild.nodeValue
-    elemento_endereco = i.getElementsByTagName("endereco")[0].firstChild.nodeValue
-    elemento_caracteristicas = i.getElementsByTagName("caracteristicas")[0].firstChild.nodeValue
-    valor = i.getElementsByTagName("valor")
+        email = j.getElementsByTagName("email")
+        data_proprietario["nome"] = nome
+        for k in email:
+            print(k.firstChild.nodeValue)
+            emails["emails"] = k
+            
+            data_proprietario["email"] = emails
+            
+        telefone = j.getElementsByTagName("telefone")[0].firstChild.nodeValue
+        for h in telefone:
+            
+            telefones["telefone"] = h
+            data_proprietario["telefone"] = telefones
+
+    data_imobiliaria.append(data_proprietario)
+    endereco = i.getElementsByTagName("endereco")
+    for a in endereco:
+        rua = a.getElementsByTagName("rua")[0].firstChild.nodeValue
+        bairro = a.getElementsByTagName("bairro")[0].firstChild.nodeValue
+        cidade = a.getElementsByTagName("cidade")[0].firstChild
+        numero = a.getElementsByTagName("número")[0].firstChild
+        endereco2["rua"] = rua
+        endereco2["bairro"] = bairro
+        endereco2["cidade"] = cidade
+        endereco2["numero"] = numero
     
+    data_imobiliaria.append(endereco)
+    caracteristicas = i.getElementsByTagName("caracteristicas")
+    for b in caracteristicas:
+        tamanho = b.getElementsByTagName("tamanho")[0].firstChild.nodeValue
+        numQuartos = b.getElementsByTagName("numQuartos")[0].firstChild.nodeValue
+        numBanheiros = b.getElementsByTagName("numBanheiros")[0].firstChild.nodeValue
+        caracteristicas2["tamanho"] = tamanho
+        caracteristicas2["numQuartos"] = numQuartos
+        caracteristicas2["numBanheiros"] = numBanheiros
+    data_imobiliaria.append(caracteristicas2)
+
+#with open("biblioteca.json", "w") as json_file:
+#    json.dump(data_imobiliaria, json_file, indent=2)
+print(data_imobiliaria)    
 
  
 
