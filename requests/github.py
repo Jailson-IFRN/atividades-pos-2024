f943050ef78c63f7151611e04fb2d0691cad1b49
Jailson <jg094151@gmail.com>
2024-08-23 14:25:13 -0300

    Exemplo - GITHUB

---
+++ requests/github.py
@@ -0,0 +1,35 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+password = "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"
+user = "Jailson-IFRN"
+
+print("Digite 1 para seguir o usuário")
+print("Digite 2 para parar de seguir o usuário ")
+
+opcao = int(input())
+
+
+if opcao == 1:
+
+    username = input("Digite o nome do usuário que deseja seguir: ")
+
+    response = requests.put(f'https://api.github.com/user/following/{username}', 
+                            auth = HTTPBasicAuth(user, password))
+
+    print(response.status_code)
+
+elif opcao == 2:
+
+    username = input("Digite o nome do usuário que deseja parar de seguir: ")
+
+    response = requests.delete(f'https://api.github.com/user/following/{username}', auth = HTTPBasicAuth(user, password))
+
+    print(response.status_code)
+
+else:
+    print("Opção não conhecida")
+
+
+
